a47980628d62d0a429c015140e9cb0b47015fe67
Kuaminika <kuaminika@gmail.com>
2025-11-17 02:17:51 -0500

    fixed table name again

--- src/repository/QueryHolder.js
+++ src/repository/QueryHolder.js
@@ -21,9 +21,9 @@ function QueryHolder() {
                                 from User u
                             left  join user_identities ui on u.id = ui.user_id
                             where u.email = ?`;
-    self.addUserQuery = `insert into user (email, name, created_at,ord_id) values (?, ?, ?, ?)`;
+    self.addUserQuery = `insert into User (email, name, created_at,ord_id) values (?, ?, ?, ?)`;
     self.addUserIdentityQuery = `insert into user_identities (user_id, provider, provider_user_id) values (?, ?, ?)`;
-    self.updateUserQuery = `update user set name = ?, email = ?  where id = ?`;
+    self.updateUserQuery = `update User set name = ?, email = ?  where id = ?`;
     self.updateUserIdentityQuery = `update user_identities set provider = ?, provider_user_id = ? where user_id = ?`;
 
 
